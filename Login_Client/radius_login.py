872d0e1887e6075d4d6a926715f05613272f5737
Israel Efrati <100380924+IsraelEfrati@users.noreply.github.com>
2022-10-16 11:02:48 +0300

    Add files via upload

--- Login_Client/radius_login.py
+++ Login_Client/radius_login.py
@@ -1,7 +1,7 @@
 import radius
 
 
-def r_login(username, password, google_auth, host_ip="192.168.25.128", secret="testing123"):
+def r_login(username, password, google_auth, host_ip="<radius server ip>", secret="testing123"):
     r = radius.Radius(secret, host=host_ip, port=1812)
     if r.authenticate(username, password+google_auth):
         return 1
